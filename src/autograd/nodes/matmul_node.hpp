35830df5da34f03f94ce76278c36376653fd24fb
bryan pakulski <BryanPakulski@outlook.com>
2026-03-26 13:02:33 +1100

    Fix batched matmul backward transpose dimensions

--- src/autograd/nodes/matmul_node.hpp
+++ src/autograd/nodes/matmul_node.hpp
@@ -18,13 +18,31 @@ struct MatmulBackward : public Node {
     Tensor grad_out = grads[0];
     Tensor grad_a;
     Tensor grad_b;
+    const int a_ndim = static_cast<int>(A.shape().size());
+    const int b_ndim = static_cast<int>(B.shape().size());
+
+    auto transpose_last_two = [](const Tensor &t) -> Tensor {
+      const int ndim = static_cast<int>(t.shape().size());
+      if (ndim < 2) {
+        return t;
+      }
+      return ops::transpose(t, ndim - 2, ndim - 1);
+    };
 
     if (next_edges.size() > 0 && next_edges[0].node) {
-      grad_a = ops::matmul(grad_out, ops::transpose(B, 0, 1));
+      if (a_ndim >= 3 || b_ndim >= 3 || grad_out.shape().size() >= 3) {
+        grad_a = ops::batched_matmul(grad_out, transpose_last_two(B));
+      } else {
+        grad_a = ops::matmul(grad_out, ops::transpose(B, 0, 1));
+      }
     }
 
     if (next_edges.size() > 1 && next_edges[1].node) {
-      grad_b = ops::matmul(ops::transpose(A, 0, 1), grad_out);
+      if (a_ndim >= 3 || b_ndim >= 3 || grad_out.shape().size() >= 3) {
+        grad_b = ops::batched_matmul(transpose_last_two(A), grad_out);
+      } else {
+        grad_b = ops::matmul(ops::transpose(A, 0, 1), grad_out);
+      }
     }
 
     return {grad_a, grad_b};
